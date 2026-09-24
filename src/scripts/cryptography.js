c268005c0690ca9e33d6e7cf72d96668013e4e05
LucaBonamino <luca.bonamino@hotmail.com>
2026-09-24 16:50:57 +0200

    remove comment

--- src/scripts/cryptography.js
+++ src/scripts/cryptography.js
@@ -76,10 +76,7 @@ export async function encryptMessage(text, publicKeyPem, senderPublicKeyPem) {
         }
         senderEncKey = toBase64(await encryptAESKey(senderRsaKey, rawKey));
     }
-    // if (senderPublicKeyPem != null) {
-    //     const senderRsaKey = await importPublicKey(senderPublicKeyPem);
-    //     senderEncKey = toBase64(await encryptAESKey(senderRsaKey, rawKey));
-    // }
+    
     return {
         encryptedKeys: {
             recipient: toBase64(encKey),
